aaea19c79dffbd17e10c90bb86bd20ddeafa49ac
Evgeniy Pak <esp23091989@gmail.com>
2018-10-19 11:18:23 +0300

    do some changes

--- app/src/main/java/com/ibis/ibisecp2/ui/viewutils/AuthenticatingWebView.java
+++ app/src/main/java/com/ibis/ibisecp2/ui/viewutils/AuthenticatingWebView.java
@@ -120,12 +120,7 @@ public class AuthenticatingWebView {
             if(error.getPrimaryError() == SslError.SSL_UNTRUSTED )
                 handler.proceed();
             else {
-                final AlertDialog.Builder builder = new AlertDialog.Builder(view.getContext());
-                builder.setMessage(R.string.notification_error_ssl_cert_invalid);
-                builder.setPositiveButton("продолжить", (dialog, which) -> handler.proceed());
-                builder.setNegativeButton("отменить", (dialog, which) -> handler.cancel());
-                final AlertDialog dialog = builder.create();
-                dialog.show();
+                handler.cancel();
             }
         }
 
